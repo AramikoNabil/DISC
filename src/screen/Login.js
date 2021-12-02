0e1fb23471d0abb7e27169159c376062ae76da34
sleepy81k <nabilnico@gmail.com>
2021-12-02 13:57:05 +0700

    + update

--- src/screen/Login.js
+++ src/screen/Login.js
@@ -86,12 +86,6 @@ const Login = () => {
     ) {
       setLoading(true);
       register(isName, isEmail, isGender, isDate)
-        // Axios.post("http://26f6-158-140-191-58.ngrok.io/api/register", {
-        //   name: isName,
-        //   email: isEmail,
-        //   gender: isGender,
-        //   birthDate: isDate,
-        // })
         .then((responseJson) => {
           const api = responseJson;
           if (api) {
@@ -103,6 +97,7 @@ const Login = () => {
               setGender("");
               setDate("");
               Go_To();
+              setLoading(false);
             }
           }
         })
