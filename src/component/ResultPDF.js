31b08453cb5aa37e402af2599466aafb433b4b78
sleepy81k <nabilnico@gmail.com>
2022-02-11 04:20:42 +0700

    optimize

--- src/component/ResultPDF.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from "react";
-
-const ResultPDF = React.forwardRef((props, ref) => {
-  return <div ref={ref}>My cool content here!</div>;
-});
-
-export default ResultPDF;
